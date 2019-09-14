9575b7329b16b109f638a66957a1410850c697d0
Andy <tutoit@gmail.com>
2019-09-14 12:15:03 +0300

    Character follows path

--- index.js
+++ index.js
@@ -55,17 +55,23 @@ class Game {
 		// loading assets
 		console.log('init');
 		this.drawMap();
+		this.hero.setPath([{
+			x: 1,
+			y: 2
+		}, {
+			x: 1,
+			y: 3
+		}, {
+			x: 2,
+			y: 3
+		}]);
 	}
 
 	drawMap = () => {
 		const currentFrameTime = Date.now();
 
 		if(!this.hero.processMovement(currentFrameTime)) {
-			if(this.shouldGo) {
-				this.hero.toTile.y -= 1; 
-			}
-
-			if(this.hero.fromTile.x !== this.hero.toTile.x || this.hero.fromTile.y !== this.hero.toTile.y) {
+			if(this.hero.fromTile.x !== this.hero.lastTile.x || this.hero.fromTile.y !== this.hero.lastTile.y) {
 				console.log(currentFrameTime);
 				this.hero.timeMoved = currentFrameTime;
 			}
@@ -84,8 +90,6 @@ class Game {
 				this.context.fillRect( x*this.tileWidth, y*this.tileHeight, this.tileWidth, this.tileHeight);
 			}
 		}
-
-		this.shouldGo = false;
 
 		this.context.fillStyle = "#0000ff";
 		this.context.fillRect(this.hero.position.x, this.hero.position.y, this.hero.characterWidth, this.hero.characterHeight);
@@ -107,39 +111,52 @@ class Character {
 		this.tileWidth = tileWidth;
 		this.tileHeight = tileHeight;
 
+		this.path = [];
+		this.lastTile = {};
 		this.placeAt(toTile);
 	}
 
 	placeAt = ({x, y}) => {
+		console.log("placed", x, y);
 		this.fromTile = {x, y};
 		this.toTile = {x, y};
 		this.position = {
-			x: (this.tileWidth * x) + ((this.tileWidth - this.characterWidth) / 2), // offset
+			x: (this.tileWidth * x) + ((this.tileWidth - this.characterWidth) / 2),
 			y: (this.tileHeight * y) + ((this.tileHeight - this.characterHeight) / 2) // offset
 		};
 	}
 
+	setPath = path => {
+		this.path = path;
+		this.lastTile = path[path.length - 1];
+	}
+
 	processMovement = time => {
-		if(this.fromTile.x === this.toTile.x  && this.fromTile.y === this.toTile.y) {
+		if(!this.path.length) return false;
+		const nextPath = this.path[0];
+
+		if(this.fromTile.x === this.lastTile.x && this.fromTile.y === this.lastTile.y) {
 			return false;
 		}
 
 		if((time - this.timeMoved) >= this.gameSpeed) {
-			this.placeAt(this.toTile);
+			this.placeAt(this.path[0]);
+			this.path.shift();
+			this.timeMoved = Date.now();
 		} else {
 			this.position = {
-				x: (this.fromTile.x * this.tileWidth) + ((this.tileWidth - this.characterWidth) / 2), // offset
+				x: (this.fromTile.x * this.tileWidth) + ((this.tileWidth - this.characterWidth) / 2),
 				y: (this.fromTile.y * this.tileHeight) + ((this.tileHeight - this.characterHeight) / 2) // offset
 			};
 
-			if(this.toTile.x !== this.fromTile.x) {
+			if(nextPath.x !== this.fromTile.x) {
 				const walked = (this.tileWidth / this.gameSpeed) * (time - this.timeMoved);
-				this.position.x += (this.toTile.x < this.fromTile.x ? 0 - walked : walked);
+				this.position.x += (nextPath.x < this.fromTile.x ? 0 - walked : walked);
 			}
 
-			if(this.toTile.y != this.fromTile.y) {
+			if(nextPath.y != this.fromTile.y) {
 				const walked = (this.tileHeight / this.gameSpeed) * (time - this.timeMoved);
-				this.position.y += (this.toTile.y < this.fromTile.y ? 0 - walked : walked);
+				this.position.y += (nextPath.y < this.fromTile.y ? 0 - walked : walked);
 			}
 		}
 
